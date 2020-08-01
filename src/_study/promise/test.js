30824ffd848b00d6e8dce741fa52a8bcc9a8768a
reason <yuany625@163.com>
2020-08-01 17:48:48 +0800

    局部变量，全局变量...

--- src/_study/promise/test.js
+++ src/_study/promise/test.js
@@ -1,16 +1,34 @@
 
 
 
-function fun1() {
-  setTimeout(()=>console.log(1))
+function test1 () {
+  function fun1() {
+    setTimeout(()=>console.log(1))
+  }
+  
+  new Promise((resolve, reject) => {
+    fun1();
+    console.log(2)
+    resolve()
+  }).then(() => {
+    console.log(3)
+  })
+  
+  console.log(4)
 }
 
-new Promise((resolve, reject) => {
-  fun1();
-  console.log(2)
-  resolve()
-}).then(() => {
-  console.log(3)
-})
+// test1(); // 2 4 3 1
 
-console.log(4)
+
+function test2() {
+  function foo() {
+    let a = b = 0; // 局部变a 全局变量b => windows.b = 0;
+    a++;
+    return a;
+  }
+  
+  foo();
+  console.log(typeof a); // => undefined
+  console.log(typeof b); // => 0
+}
+test2();
