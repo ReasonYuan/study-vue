104ff153c84962109dc3f42ab383a063c8583f67
reason <yuany625@163.com>
2020-08-03 16:52:28 +0800

    插件记载...

--- src/_study/test/test2.js
+++ src/_study/test/test2.js
@@ -1,12 +1,17 @@
 /*
  * @Author: reason
  * @Date: 2020-08-03 11:20:05
- * @LastEditTime: 2020-08-03 15:08:52
+ * @LastEditTime: 2020-08-03 16:18:22
  * @FilePath: /study-vue/src/_study/test/test2.js
  * @Descripttion: 
  */
 
- // 插件 JavaScript Booster
+ // 插件 KoroFileHeader 文件头部注释
+ // 插件 Polacode 代码截图
+ // 插件 JavaScript Booster 优雅代码提示
+ // 插件 Browser Preview VSCode内打开浏览器
+ // 插件 Better Comments 高亮注释
+ // 插件 REST Client -> 接口查看
 
 const str = `baidu.com`;
 
