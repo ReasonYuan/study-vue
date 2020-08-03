00ac9cd36f81d26126f76887509ce929eb628330
reason <yuany625@163.com>
2020-08-03 15:09:15 +0800

    VSCode JavaScript Booster 插件

--- src/_study/test/test2.js
+++ src/_study/test/test2.js
@@ -1,7 +1,19 @@
 /*
  * @Author: reason
  * @Date: 2020-08-03 11:20:05
- * @LastEditTime: 2020-08-03 11:34:10
+ * @LastEditTime: 2020-08-03 15:08:52
  * @FilePath: /study-vue/src/_study/test/test2.js
  * @Descripttion: 
  */
+
+ // 插件 JavaScript Booster
+
+const str = `baidu.com`;
+
+const get = str => {
+  console.log('str:', str);
+  return `hi ${str}`;
+}
+
+let a = 33;
+str = a === 33 ? '刘亦菲' : '佟丽娅';
