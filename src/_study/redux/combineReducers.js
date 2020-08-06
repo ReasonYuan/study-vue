1f62e8159ace8dbc898fbbd0042c29b78a8e498e
reason <yuany625@163.com>
2020-08-06 20:37:26 +0800

    修复combineReducers

--- src/_study/redux/combineReducers.js
+++ src/_study/redux/combineReducers.js
@@ -1,16 +1,16 @@
 /*
  * @Author: reason
  * @Date: 2020-08-06 17:11:35
- * @LastEditTime: 2020-08-06 17:16:33
+ * @LastEditTime: 2020-08-06 20:35:47
  * @FilePath: /study-vue/src/_study/redux/combineReducers.js
  * @Descripttion: combineReducers
  */
 
 const combineReducers = (reducers) => {
 
-  const finalState = { };
-  const reducer = (state, action) => {
-
+  
+  const reducer = (state={}, action) => {
+    const finalState = { };
     for (const p in reducers) {
       finalState[p] = reducers[p](state[p], action);
     }
