f5d9c3594ebcf626244ae037fe11782251968ca8
reason <yuany625@163.com>
2020-08-07 14:49:51 +0800

    添加redux中间件

---
+++ src/_study/redux/applyMiddleware.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: reason
+ * @Date: 2020-08-07 14:46:48
+ * @LastEditTime: 2020-08-07 14:47:06
+ * @FilePath: /study-vue/src/_study/redux/applyMiddleware.js
+ * @Descripttion: redux 中间件
+ */
+
+export default function applyMiddleware(...middlewares) {
+  return createStore => (...args) => {
+    // 利用传入的createStore和reducer和创建一个store
+    const store = createStore(...args)
+    let dispatch = () => {
+      throw new Error(
+      )
+    }
+    const middlewareAPI = {
+      getState: store.getState,
+      dispatch: (...args) => dispatch(...args)
+    }
+    // 让每个 middleware 带着 middlewareAPI 这个参数分别执行一遍
+    const chain = middlewares.map(middleware => middleware(middlewareAPI))
+    // 接着 compose 将 chain 中的所有匿名函数，组装成一个新的函数，即新的 dispatch
+    dispatch = compose(...chain)(store.dispatch)
+    return {
+      ...store,
+      dispatch
+    }
+  }
+}
