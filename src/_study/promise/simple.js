3925f12f95527290cb536a72254f825223d1b791
reason <yuany625@163.com>
2020-09-03 19:40:34 +0800

    加入测试用例

--- src/_study/promise/simple.js
+++ src/_study/promise/simple.js
@@ -1,11 +1,12 @@
 /*
  * @Author: reason
  * @Date: 2020-07-08 13:51:48
- * @LastEditTime: 2020-09-03 19:17:32
+ * @LastEditTime: 2020-09-03 19:40:27
  * @FilePath: /study-vue/src/_study/promise/simple.js
  * @Descripttion: 
  */
 
+ 
 export default {
 }
 
