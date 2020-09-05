289d29d7f90657c5df1590ddc029876ad940e69a
reason <yuany625@163.com>
2020-09-05 18:09:37 +0800

    like this

--- src/_study/promise/simple.js
+++ src/_study/promise/simple.js
@@ -1,10 +1,11 @@
 /*
  * @Author: reason
  * @Date: 2020-07-08 13:51:48
- * @LastEditTime: 2020-09-05 18:06:38
+ * @LastEditTime: 2020-09-05 18:09:21
  * @FilePath: /study-vue/src/_study/promise/simple.js
  * @Descripttion: 
  */
+ 
  
 export default {
 }
