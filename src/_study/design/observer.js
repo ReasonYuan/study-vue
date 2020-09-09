c2f644e80f4bfc47021baf760bbdbe2f25f3482c
reason <yuany625@163.com>
2020-09-09 10:17:53 +0800

    update

--- src/_study/design/observer.js
+++ src/_study/design/observer.js
@@ -1,11 +1,12 @@
 /*
  * @Author: reason
  * @Date: 2020-09-08 17:39:39
- * @LastEditTime: 2020-09-09 09:39:19
+ * @LastEditTime: 2020-09-09 10:17:47
  * @FilePath: /study-vue/src/_study/design/observer.js
  * @Descripttion: 观察者模式
  */
 
+ 
 const observer = {
   subscribes: [],
 
