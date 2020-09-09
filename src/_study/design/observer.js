2183dc86d54e42c4d4155942250344bec3658b57
reason <yuany625@163.com>
2020-09-09 15:25:56 +0800

    优化代码

--- src/_study/design/observer.js
+++ src/_study/design/observer.js
@@ -1,11 +1,12 @@
 /*
  * @Author: reason
  * @Date: 2020-09-08 17:39:39
- * @LastEditTime: 2020-09-09 10:17:47
+ * @LastEditTime: 2020-09-09 15:25:46
  * @FilePath: /study-vue/src/_study/design/observer.js
  * @Descripttion: 观察者模式
  */
 
+ 
  
 const observer = {
   subscribes: [],
