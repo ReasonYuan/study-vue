66787941dbfd55d3237b86e9e688c956593b6048
reason <yuany625@163.com>
2020-09-09 09:39:27 +0800

    观察者模式

---
+++ src/_study/design/observer.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: reason
+ * @Date: 2020-09-08 17:39:39
+ * @LastEditTime: 2020-09-09 09:39:19
+ * @FilePath: /study-vue/src/_study/design/observer.js
+ * @Descripttion: 观察者模式
+ */
+
+const observer = {
+  subscribes: [],
+
+}
