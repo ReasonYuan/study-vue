8e34457334603dc7062633110e0d2433b0d25fce
reason <yuany625@163.com>
2020-09-10 15:15:13 +0800

    享员模式

--- src/_study/design/templateMethod.js
+++ src/_study/design/templateMethod.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: reason
+ * @Date: 2020-09-08 17:39:39
+ * @LastEditTime: 2020-09-09 15:25:46
+ * @FilePath: /study-vue/src/_study/design/observer.js
+ * @Descripttion: 模板方法模式
+ */
+
 // 抽象出的员工类
 class Staff {
     constructor(name) {
