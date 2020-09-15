7ba3528ba447160a03474b63e25e79efc0a27c91
reason <yuany625@163.com>
2020-09-15 17:38:16 +0800

    享元模式

---
+++ src/_study/design/flyweight.js
@@ -0,0 +1,42 @@
+/*
+ * @Author: reason
+ * @Date: 2020-09-14 10:13:28
+ * @LastEditTime: 2020-09-15 17:14:18
+ * @FilePath: /study-vue/src/_study/design/flyweight.js
+ * @Descripttion: 享元模式
+ */
+// 共享部分
+const FlyWeight = {
+  moveX: function(x) {
+      this.x = x;
+  },
+  moveY: function(y) {
+      this.y = y;
+  }
+}
+// 玩家
+const Player = function(x, y, color) {
+  this.x = x;
+  this.y = y;
+  this.color = color;
+}
+Player.prototype = FlyWeight
+
+// 怪兽
+const Monster = function() {
+  this.x = 10;
+  this.y = 30;
+  this.call = '嗷嗷~';
+  this.name = '哥斯拉';
+}
+Monster.prototype = FlyWeight;
+
+// 测试
+const player = new Player(30, 10, '#333')
+player.moveX(5); player.moveY(-10)
+
+const monster = new Monster()
+monster.moveY(-4); monster.moveY(10)
+
+console.log(player)
+console.log(monster)
