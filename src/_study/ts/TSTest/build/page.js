4d9061c9b42feac13426dd407500d27a070f29a6
reason <yuany625@163.com>
2020-10-07 14:26:38 +0800

    parcel打包

---
+++ src/_study/ts/TSTest/build/page.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+ * @Author: reason
+ * @Date: 2020-10-07 14:19:39
+ * @LastEditTime: 2020-10-07 14:21:00
+ * @FilePath: /study-vue/src/_study/ts/TSTest/src/page.ts
+ * @Descripttion:
+ */
+var teacher = '悟空';
+console.log(teacher);
