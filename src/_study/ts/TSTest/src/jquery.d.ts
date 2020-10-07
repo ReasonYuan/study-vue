348607b351691a09ee724b1359702341d7f1c219
reason <yuany625@163.com>
2020-10-07 14:37:34 +0800

    使用jquery

---
+++ src/_study/ts/TSTest/src/jquery.d.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: reason
+ * @Date: 2020-10-07 14:35:03
+ * @LastEditTime: 2020-10-07 14:36:23
+ * @FilePath: /study-vue/src/_study/ts/TSTest/src/jquery.d.ts
+ * @Descripttion: 
+ */
+
+
+
+ // npm i @types/jquery
+
+// 写 ts 申明文件
