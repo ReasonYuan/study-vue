706eaf6c3a2eb1a40569fc13197b57518058b7d5
reason <yuany625@163.com>
2020-10-07 17:12:31 +0800

    生命周期

--- src/_study/vue3/vue3-1/src/components/SelectGirl.vue
+++ src/_study/vue3/vue3-1/src/components/SelectGirl.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-10-07 16:38:58
+ * @LastEditTime: 2020-10-07 17:11:33
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/SelectGirl.vue
  * @Descripttion: setup 和 ref 函数
 -->
@@ -33,8 +33,6 @@
     </button>
     <div>您选择了 {{selectGirl}} 为你服务</div>
   </div>
-
-
   </div>
 </template>
 
@@ -56,7 +54,7 @@
 
 
 // ===== reactive =====
-import { reactive, toRefs } from "vue"
+import { reactive, toRefs, onBeforeMount, onMounted, onBeforeUpdate, onUpdated } from "vue"
 
 interface DataProps {
   girls: string[];
@@ -67,19 +65,65 @@ interface DataProps {
 export default {
   name: "SelectGirl",
   setup() {
+    console.log('1 - 开始创建组件：setup')
     const data: DataProps = reactive({
       girls: ['刘亦菲', '佟亚丽'],
       selectGirl: '',
       selectGirlFun: (index: number) => {
         data.selectGirl = data.girls[index]
       }
-    })
+    });
     // return { data }
+
+    onBeforeMount(() => {
+      console.log('2 - 组件挂载到页面之前执行：onBeforeMount')
+    });
+
+    onMounted(() => {
+      console.log('3 - 组件挂载到页面之后执行：onMounted')
+    });
+
+    onBeforeUpdate(() => {
+      console.log('4 - 组件更新之前执行：onBeforeUpdate')
+    });
+
+    onUpdated(() => {
+      console.log('5 - 组件更新之后执行：onUpdated')
+    })
+
 
     // ===== toRefs =====
     const refData = toRefs(data)
     return { ...refData }
+  },
+
+  beforeCreate() {
+    console.log('#1.1 - 组件创建之前：beforeCreate')
+  },
+  beforeMount() {
+    console.log('#2.1 - 组件挂载到页面之前执行：beforeMount')
+  },
+  mounted() {
+    console.log('#3.1 - 组件挂载到页面之后执行：mounted')
+  },
+  beforeUpdate() {
+    console.log('#4.1 - 组件更新之前执行：beforeUpdate')
+  },
+  updated() {
+    console.log('#5.1 - 组件更新之后执行：updated')
   }
+  // beforeDestroy => onBeforedUnmount
+  // destroyed => onUnmounted
 }
+
+// 生命周期
+// setup
+// beforeCreate created
+
+// onBeforeUnmount()  // 组件卸载之前执行 <= beforeDestroy
+// onUnmounted()      // 组件销毁之后执行 <= destroyed
+// onActivated()      // 组件激活时，在组件<keep-alive></keep-alive>内存在
+// onDeactivated()    // 组件未激活时（到后台时），在组件<keep-alive></keep-alive>内存在
+// onErrorCaptured    // 捕获子组件异常时
 
 </script>
