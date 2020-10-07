2af2b0cf18c4a0a15a0a5d2e7788586299445cc1
reason <yuany625@163.com>
2020-10-07 16:24:43 +0800

    setup和ref函数

--- src/_study/vue3/vue3-1/src/App.vue
+++ src/_study/vue3/vue3-1/src/App.vue
@@ -1,18 +1,30 @@
+<!--
+ * @Author: reason
+ * @Date: 2020-10-07 15:36:40
+ * @LastEditTime: 2020-10-07 16:22:51
+ * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/App.vue
+ * @Descripttion: 
+-->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <SelectGirl />
+  <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
+  </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+// import HelloWorld from './components/HelloWorld.vue';
+import SelectGirl from './components/SelectGirl.vue';
 
 @Options({
   components: {
-    HelloWorld,
+    // HelloWorld,
+    SelectGirl
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue { }
 </script>
 
 <style>
---
+++ src/_study/vue3/vue3-1/src/components/SelectGirl.vue
@@ -0,0 +1,35 @@
+<!--
+ * @Author: reason
+ * @Date: 2020-10-07 16:19:16
+ * @LastEditTime: 2020-10-07 16:23:27
+ * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/SelectGirl.vue
+ * @Descripttion: 
+-->
+<template>
+  <div>
+  <h2>Hello World</h2>
+  <div>
+    <button v-for="(item, index) in girls" v-bind:key="index" @click="() => selectGirlFun(index)">
+      {{index}}:{{item}}
+    </button>
+    <div>您选择了 {{selectGirl}} 为你服务</div>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+export default defineComponent({
+  name: "SelectGirl",
+  setup() {
+    const girls = ref(['刘亦菲', '佟亚丽'])
+    const selectGirl = ref('');
+    const selectGirlFun = (index: number) => {
+      selectGirl.value = girls.value[index]
+    }
+    return {
+      girls, selectGirl, selectGirlFun
+    }
+  }
+})
+</script>
