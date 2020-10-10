f7779462e2c8fd8f1408ce658137f00c90eaf5ed
reason <yuany625@163.com>
2020-10-10 11:04:39 +0800

    for in， for of

---
+++ src/_study/react/react.js
@@ -0,0 +1,50 @@
+/*
+ * @Author: reason
+ * @Date: 2020-10-10 10:17:42
+ * @LastEditTime: 2020-10-10 10:31:24
+ * @FilePath: /study-vue/src/_study/react/react.js
+ * @Descripttion: 手写react
+ */
+
+
+
+const React = {
+  createElement(tag, attrs, ...children) {
+    return {
+      tag, attrs, children
+    }
+  }
+}
+
+
+const ReactDom = {
+  render (vdom, container) {
+    container.innerHTML = ""
+    _render(vdom, container)
+  }
+}
+
+_render(vdom, container) {
+  let node;
+  if (typeof vdom === 'string') {
+    node = document.createTextNode(vdom)
+  } else if (typeof vdom === 'object') {
+    node = document.createElement(vdom.tag);
+    setAttribute(node, vdom.attrs);
+    vdom.children.forEach((childVdom) => render(childVdom, node));
+  }
+}
+
+etAttribute(node, attrs) {
+  for (let key in attrs) {
+    if (key.startsWith("on")) {
+      node[key.toLocaleLowerCase()] = attrs[key]
+    } else if (key === "style") {
+      Object.assign(node.style, attrs[key]);
+    } else {
+      node[key] = attrs[key]
+    }
+  }
+}
+
+export default React
---
+++ src/_study/react/test.js
@@ -0,0 +1,45 @@
+/*
+ * @Author: reason
+ * @Date: 2020-10-10 10:17:32
+ * @LastEditTime: 2020-10-10 11:04:23
+ * @FilePath: /study-vue/src/_study/react/test.js
+ * @Descripttion: 
+ */
+
+// import React from './react'
+
+// const div = <div className="hello">hello</div>
+
+// React.createElement("div", {className: "header"}, "hello")
+
+
+/*
+const array = ['a', 'b', 'c', 'd', 'e', 'f']
+const map = {
+  name: '张三',
+  gender: '男',
+  age: 18
+}
+
+// for in 遍历的key
+console.log('====== in =====')
+for (let item in array) {
+  console.log('---------array item:', item)
+}
+console.log('---- map --->')
+for (let item in map) {
+  console.log('---------map:', item)
+}
+
+
+// for of 只能数组用，用于遍历数组
+console.log('\n====== of =====')
+for (let item of array) {
+  console.log('---------array item:', item)
+}
+console.log('---- map --->')
+for (let item of map) {
+  console.log('---------map item:', item)
+}
+*/
+
