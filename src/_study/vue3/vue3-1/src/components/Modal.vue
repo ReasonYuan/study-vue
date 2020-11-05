12a22c753e50e2a2f28ac23f49798b893216b66f
reason <yuany625@163.com>
2020-11-05 17:17:40 +0800

    vue3: teleport瞬移组件

---
+++ src/_study/vue3/vue3-1/src/components/Modal.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: reason
+ * @Date: 2020-11-05 17:02:28
+ * @LastEditTime: 2020-11-05 17:17:18
+ * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Modal.vue
+ * @Descripttion: Teleport - 瞬移组件，解决问题：1、逻辑干扰；2、样式混乱
+-->
+<template>
+  <teleport to="#modal">
+    <div id="center">
+      <h2>Welcome</h2>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+export default {
+   name: "modal",
+}
+</script>
+
+<style scoped>
+  #center {
+    width: 200px;
+    height: 200px;
+    border: 2px solid black;
+    background: white;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    margin-left: -100px;
+    margin-top: -100px;
+  }
+</style>
--- src/_study/vue3/vue3-1/src/components/RadomGirl.vue
+++ src/_study/vue3/vue3-1/src/components/RadomGirl.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: reason
  * @Date: 2020-11-02 14:20:29
- * @LastEditTime: 2020-11-03 09:24:33
+ * @LastEditTime: 2020-11-05 17:09:13
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/RadomGirl.vue
  * @Descripttion: 远程获取数据，随机出现美女
 -->
@@ -11,6 +11,7 @@
   <div>系统将随机选择一位美女为您服务:</div>
   <div v-if="loading">Loading...</div>
   <img v-if="loaded" :src="result.imgUrl" />
+  <modal />
 </template>
 
 <script lang="ts">
@@ -18,6 +19,7 @@
 // ===== reactive =====
 // import { ref, reactive, toRefs, onBeforeMount, onMounted, onBeforeUpdate, onUpdated, onRenderTracked, onRenderTriggered, watch } from "vue"
 import useUrlAxios from '../hooks/useURLAxios'
+import modal from './Modal.vue'
 
 interface DataProps {
   girls: string[];
@@ -27,6 +29,7 @@ interface DataProps {
 
 export default {
   name: "Timer",
+  components: { modal },
   setup() {
     const { result, loading, loaded } = useUrlAxios('https://apiblog.jspang.com/default/getGirl')
     return { result, loading, loaded }
