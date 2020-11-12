3d57926ffc22ac38859b45d8c1731f9d35a051ed
reason <yuany625@163.com>
2020-11-12 08:53:41 +0800

    restore

--- src/_study/vue3/vue3-1/src/components/SelectGirl.vue
+++ src/_study/vue3/vue3-1/src/components/SelectGirl.vue
@@ -1,11 +1,10 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-12 08:53:02
+ * @LastEditTime: 2020-11-12 08:53:35
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/SelectGirl.vue
  * @Descripttion: setup 和 ref 函数
 -->
-
 
 <template>
   <div>
