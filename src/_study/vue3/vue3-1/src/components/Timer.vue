821f95bb4c708b88702e4f444cf7282a1e9a27db
reason <yuany625@163.com>
2020-11-17 14:12:10 +0800

    new line

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,10 +1,11 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-17 14:11:41
+ * @LastEditTime: 2020-11-17 14:12:02
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
+
 
 <template>
   <div>{{nowTime}}</div>
