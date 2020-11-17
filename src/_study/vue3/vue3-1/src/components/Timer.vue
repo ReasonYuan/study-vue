4b5967b392c05a047cce1d9ef30de7696e9bd5c4
reason <yuany625@163.com>
2020-11-17 14:12:26 +0800

    优化代码

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,11 +1,10 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-17 14:12:02
+ * @LastEditTime: 2020-11-17 14:12:18
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
-
 
 <template>
   <div>{{nowTime}}</div>
