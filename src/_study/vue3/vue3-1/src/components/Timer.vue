4f43a312ef3ec4d1d7ba05ef899e4c2720d8e95b
reason <yuany625@163.com>
2020-11-19 19:04:46 +0800

    fix bug

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,10 +1,11 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-19 19:04:23
+ * @LastEditTime: 2020-11-19 19:04:40
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
+
 
 
 <template>
