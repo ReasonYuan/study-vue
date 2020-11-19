f1dda0fb9b8b44b70a7152372db2c061d569b6a4
reason <yuany625@163.com>
2020-11-19 19:04:37 +0800

    restore

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,11 +1,10 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-19 19:04:10
+ * @LastEditTime: 2020-11-19 19:04:23
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
-
 
 
 <template>
