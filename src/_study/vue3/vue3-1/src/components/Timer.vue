b62a88a9db9626c15bbcd2a3db890a1cdad3f205
reason <yuany625@163.com>
2020-11-20 11:21:45 +0800

    update

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,11 +1,10 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-19 19:04:40
+ * @LastEditTime: 2020-11-20 11:21:41
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
-
 
 
 <template>
