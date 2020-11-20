a2eb4564ee7934377b75d42eb566d401a83d683c
reason <yuany625@163.com>
2020-11-20 11:22:10 +0800

    优化

--- src/_study/vue3/vue3-1/src/components/Timer.vue
+++ src/_study/vue3/vue3-1/src/components/Timer.vue
@@ -1,11 +1,10 @@
 <!--
  * @Author: reason
  * @Date: 2020-10-07 16:19:16
- * @LastEditTime: 2020-11-20 11:21:51
+ * @LastEditTime: 2020-11-20 11:22:02
  * @FilePath: /study-vue/src/_study/vue3/vue3-1/src/components/Timer.vue
  * @Descripttion: 模块化
 -->
-
 <template>
   <div>{{nowTime}}</div>
   <div><button @click="getNowTime">计时开始</button></div>
